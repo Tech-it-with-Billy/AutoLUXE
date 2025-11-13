b95378849fcbb971068b0058b19775020532cb88
Billy Och <ochiengbilly02@gmail.com>
2025-11-13 13:09:42 +0300

    Building and creating route link  to the About page

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Index.css";
+import NavBar from "./NavBar";
+import Services from "./Services";
+import Footer from "./Footer";
+
+function About () {
+    return (
+        <div>
+            <NavBar />
+            <Services />
+            <Footer />
+        </div>
+    )
+}
+
+export default About;
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import About from './components/About.jsx'
 
 const router = createBrowserRouter([
   {path: '/', element: <App />},
+  {path: '/about', element: <About />}
+
 ])
 
 createRoot(document.getElementById('root')).render(
