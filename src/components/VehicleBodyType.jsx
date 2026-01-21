a9feaffa03b8a9dddafbd475d1e4f7b29d8924ca
Billy Och <ochiengbilly02@gmail.com>
2026-01-21 19:56:59 +0300

    Adding filtering by brand and body type

---
+++ src/components/VehicleBodyType.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import VehicleFilterIcon from './VehicleFilterIcon';
+
+function VehicleBodyType() {
+    return (
+        <div className='flex flex-col'>
+            <div className='flex justify-between p-15'>
+                <h2>Rent by body type</h2>
+                <button className='flex gap-3 items-center'>
+                    View all
+                    <img src="/images/ArrowBlack.png" alt="" className='h-3 ' />
+                </button>
+            </div>
+            <div className='grid grid-cols-6 px-15 gap-5'>
+                <VehicleFilterIcon image='/images/bodytype/compact.png' />
+                <VehicleFilterIcon image='/images/bodytype/convertible.png' />
+                <VehicleFilterIcon image='/images/bodytype/coup.png' />
+                <VehicleFilterIcon image='/images/bodytype/crossover.png' />
+                <VehicleFilterIcon image='/images/bodytype/family.png' />
+                <VehicleFilterIcon image='/images/bodytype/lemo.png' />
+                <VehicleFilterIcon image='/images/bodytype/sedan.png' />
+                <VehicleFilterIcon image='/images/bodytype/sports.png' />
+                <VehicleFilterIcon image='/images/bodytype/suv.png' />
+                <VehicleFilterIcon image='/images/bodytype/truck.png' />
+                <VehicleFilterIcon image='/images/bodytype/wagon.png' />
+                <VehicleFilterIcon image='/images/bodytype/sports.png' />
+            </div>
+        </div>
+    )
+}
+
+export default VehicleBodyType;
---
+++ src/components/VehicleFilterIcon.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VehicleFilterIcon({image}) {
+    return (
+        <div>
+            <img src={image} className='' alt="" />
+        </div>
+    )
+}
+
+export default VehicleFilterIcon
