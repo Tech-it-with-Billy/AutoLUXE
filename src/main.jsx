030ec276dcccd14f6c1c7f5d3532172924adcc4a
Billy Och <ochiengbilly02@gmail.com>
2026-01-21 08:49:19 +0300

    Adding image files in the public folder

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-  {path: '', element: <App />}
+  {path: '/', element: <App />}
 ])
 
 createRoot(document.getElementById('root')).render(
