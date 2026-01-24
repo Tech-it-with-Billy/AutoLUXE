d15c617676d4134fdda2e625b804e36fbe545bf8
Billy Och <ochiengbilly02@gmail.com>
2026-01-24 22:56:31 +0300

    Implementing the messaging logic

---
+++ src/components/MessageStore.jsx
@@ -0,0 +1,26 @@
+import { create } from 'zustand';
+
+export const useMessageStore = create((set) => ({
+    message: {
+        name: '',
+        email: '',
+        message: '',
+    },
+
+    setMessage: (data) => 
+        set((state) => ({
+            message: {
+                ...state.message, ...data
+            }
+        })),
+    
+    resetMessage: () => 
+        set({
+            message: {
+                name: '',
+                email: '',
+                message: '',
+            },
+        })
+
+}))
